6b724a58386ab20676a08e92684394dfe419cc43
janismozgis-rcs <35929587+janismozgis-rcs@users.noreply.github.com>
2022-02-21 21:05:38 +0200

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -15,9 +15,7 @@ function Home() {
                 </p>
             </div>
             <div className="card-deck mb-3 text-center">
-                <Card title={cards[0].title} price={cards[0].price} line1={cards[0].line1} line2={cards[0].line2} line3={cards[0].line3} line4={cards[0].line4} button={cards[0].button}/> 
-                <Card title={cards[1].title} price={cards[1].price} line1={cards[1].line1} line2={cards[1].line2} line3={cards[1].line3} line4={cards[1].line4} button={cards[1].button}/>
-                <Card title={cards[2].title} price={cards[2].price} line1={cards[2].line1} line2={cards[2].line2} line3={cards[2].line3} line4={cards[2].line4} button={cards[2].button}/>
+                {cardsList}
             </div>
         </div>
     )
